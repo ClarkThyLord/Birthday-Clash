a966c0c760cd6d79f3b332e2bf5ba0fd04f54fc5
Christian Cuevas <ccpotential@gmail.com>
2020-05-05 13:31:10 -0700

    walking people
    Clark was here :V

--- src/index.js
+++ src/index.js
@@ -3,30 +3,51 @@ import logoImg from "./assets/icon.svg";
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
+  },
+  scale: {
+      mode: Phaser.Scale.RESIZE
   }
 };
 
 const game = new Phaser.Game(config);
 
+
+var people = [];
+
 function preload() {
-  this.load.image("icon", logoImg);
+  this.load.spritesheet('person.walking', 'src/assets/person.walking.png', { frameWidth: 37, frameHeight: 45, endFrame: 17 });
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "icon");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
+  this.anims.create({
+    key: 'run',
+    frames: 'person.walking',
+    frameRate: 20,
+    repeat: -1
   });
+
+  for (let p = 0; p < 25; p++)
+  {
+    let person = this.add.sprite(
+      Math.floor(Math.random() * this.cameras.main.width),
+      Math.floor(Math.random() * this.cameras.main.height),
+      'person.walking').play('run').setInteractive();
+    
+    person.setData('direction', Math.random());
+
+    people.push(person);
+  }
+}
+
+function update(time, delta) {
+  people.forEach(function (person, index) {
+    person.x += Math.cos(person.getData('direction')) * 0.1 * delta;
+    person.y += Math.sin(person.getData('direction')) * 0.1 * delta;
+  });
+
+  Phaser.Actions.WrapInRectangle(people, Phaser.Geom.Rectangle.Clone(this.cameras.main), 45);
 }
