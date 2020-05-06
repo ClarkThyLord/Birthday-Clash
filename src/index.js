d4ee78f7a681cd3135b4f05c40fcdb709be41a91
Christian Cuevas <ccpotential@gmail.com>
2020-05-06 00:27:42 -0700

    added graph
    Clark was here :V

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import Phaser from "phaser";
-import * as dat from 'dat.gui';
+import * as dat from "dat.gui";
+import {
+  Chart
+} from "chart.js";
 
 
 
 var hint;
 var solved = false;
+
+var chart;
 
 var people = [];
 
@@ -13,15 +18,15 @@ function spawn_person(scene) {
   let person = scene.add.sprite(
     Math.floor(Math.random() * scene.cameras.main.width),
     Math.floor(Math.random() * scene.cameras.main.height),
-    'person.walking').play('walking').setInteractive();
+    "person.walking").play("walking").setInteractive();
 
   var direction = Math.floor(Math.random() * 361);
-  person.setData('direction', direction);
+  person.setData("direction", direction);
   person.angle = direction;
   person.depth = 0;
 
-  var color = Math.random() * 0xffffff;
-  person.setData('color', color);
+  var color = Math.floor(Math.random() * 0xffffff);
+  person.setData("color", color);
   person.setTint(color);
 
   var birthday = new Date(
@@ -29,7 +34,7 @@ function spawn_person(scene) {
     Math.floor(Math.random() * 12) + 1,
     Math.floor(Math.random() * 31) + 1
   );
-  person.setData('birthday', birthday);
+  person.setData("birthday", birthday);
 
   people.push(person);
 
@@ -55,7 +60,8 @@ gui.add(window, "walking_speed", 0.01, 1.0, 0.01);
 window.amount_of_people = 32;
 gui.add(window, "amount_of_people", 2, 1000, 1).onChange(function (amount) {
   solved = false;
-  people.forEach((person, index) => person.setTint(person.getData('color')));
+  if (typeof (chart) == 'object') chart.destroy();
+  people.forEach((person, index) => person.setTint(person.getData("color")));
   while (amount != people.length) {
     if (amount > people.length) {
       spawn_person(scene);
@@ -71,35 +77,70 @@ window.solve_paradox = function () {
   walking = false;
   var birthdays = {};
   people.forEach(function (person, index) {
-    var birthday = person.getData("birthday");
-    if (typeof (birthdays[birthday]) == 'undefined') {
+    var birthday = person.getData("birthday").toLocaleDateString(
+      undefined, {
+        month: "long",
+        day: "numeric"
+      }
+    );
+
+    if (typeof (birthdays[birthday]) == "undefined") {
       birthdays[birthday] = person;
       person.setTint(0x808080);
       return;
-    } else if (typeof (birthdays[birthday]) == 'object') {
-      let color = Math.random() * 0xffffff;
+    } else if (Array.isArray((birthdays[birthday])))
+      birthdays[birthday][0] += 1;
+    else {
+      let color = Math.floor(Math.random() * 0xffffff);
       birthdays[birthday].depth = 2;
       birthdays[birthday].setTint(color);
       birthdays[birthday] = [2, color];
-    } else birthdays[birthday][0] += 1;
+    }
     person.setTint(birthdays[birthday][1]);
     person.depth = 2;
   });
 
-  var birthdays_repeated = {
-    other: 0
-  }
+
+  let labels = ["Other"]
+  let colors = ["#32d115"]
+  let data = [0]
   this.Object.keys(birthdays).forEach(function (birthday, index) {
-    if (birthdays[birthday] > 1) birthdays_repeated[birthday] = birthdays[birthday];
-    else birthdays_repeated["other"] += 1;
+    if (Array.isArray(birthdays[birthday])) {
+      labels.push(birthday);
+      colors.push("#" + Math.floor(birthdays[birthday][1]).toString(16));
+      data.push(birthdays[birthday][0]);
+    } else data[0] += 1;
   });
 
 
+  if (typeof (chart) == 'object') chart.destroy();
+  chart = new Chart(document.getElementById("chart-canvas"), {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "# people",
+        data: data,
+        backgroundColor: colors,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }
 gui.add(window, "solve_paradox");
 
 window.refresh_population = function () {
   solved = false;
+  if (typeof (chart) == 'object') chart.destroy();
   while (people.length > 0) despawn_person(people.length - 1);
   while (people.length < amount_of_people) spawn_person(scene);
 }
@@ -126,7 +167,7 @@ const game = new Phaser.Game({
 
 
 function preload() {
-  this.load.spritesheet('person.walking', 'src/assets/person.walking.png', {
+  this.load.spritesheet("person.walking", "src/assets/person.walking.png", {
     frameWidth: 32,
     frameHeight: 32,
     endFrame: 0
@@ -136,17 +177,18 @@ function preload() {
 
 function create() {
   scene = this;
+  game.canvas.id = "main-canvas";
 
   this.anims.create({
-    key: 'walking',
-    frames: 'person.walking',
+    key: "walking",
+    frames: "person.walking",
     frameRate: 20,
     repeat: -1
   });
 
-  hint = this.add.text(32, 32, 'Click on someone!', {
-    font: '16px Courier',
-    fill: '#00ff00'
+  hint = this.add.text(32, 32, "Click on someone!", {
+    font: "16px Courier",
+    fill: "#00ff00"
   });
   hint.depth = 2;
 
@@ -156,23 +198,23 @@ function create() {
   }
 
 
-  this.input.on('gameobjectmove', function (pointer, person) {
-    hint.setText(person.getData('birthday').toLocaleDateString(
+  this.input.on("gameobjectmove", function (pointer, person) {
+    hint.setText(person.getData("birthday").toLocaleDateString(
       undefined, {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
       }
     ));
 
     if (!solved) person.setTint(0xa3f3ff);
   });
 
-  this.input.on('gameobjectout', function (pointer, person) {
+  this.input.on("gameobjectout", function (pointer, person) {
     hint.setText("");
 
-    if (!solved) person.setTint(person.getData('color'));
+    if (!solved) person.setTint(person.getData("color"));
   });
 }
 
@@ -180,8 +222,8 @@ function create() {
 function update(time, delta) {
   if (walking) {
     people.forEach(function (person, index) {
-      person.x += Math.cos(person.getData('direction') * Math.PI / 180) * window.walking_speed * delta;
-      person.y += Math.sin(person.getData('direction') * Math.PI / 180) * window.walking_speed * delta;
+      person.x += Math.cos(person.getData("direction") * Math.PI / 180) * window.walking_speed * delta;
+      person.y += Math.sin(person.getData("direction") * Math.PI / 180) * window.walking_speed * delta;
     });
 
     Phaser.Actions.WrapInRectangle(people, Phaser.Geom.Rectangle.Clone(this.cameras.main), 45);
