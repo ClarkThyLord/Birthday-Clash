ef748d3c75601d2d2c74025d6af2b4ba542975a9
Christian Cuevas <ccpotential@gmail.com>
2020-05-05 18:36:49 -0700

    added dat.gui control
    Clark was here :V

--- src/index.js
+++ src/index.js
@@ -1,9 +1,64 @@
 import Phaser from "phaser";
+import * as dat from 'dat.gui';
+
+
 
 var hint;
-var selected;
 
 var people = [];
+
+
+function spawn_person(scene) {
+  let person = scene.add.sprite(
+    Math.floor(Math.random() * scene.cameras.main.width),
+    Math.floor(Math.random() * scene.cameras.main.height),
+    'person.walking').play('walking').setInteractive();
+
+  var direction = Math.floor(Math.random() * 361);
+  person.setData('direction', direction);
+  person.angle = direction;
+  person.depth = 0;
+
+  var color = Math.random() * 0xffffff;
+  person.setData('color', color);
+  person.setTint(color);
+
+  var birthday = new Date(
+    Math.floor(Math.random() * 69) + 1950,
+    Math.floor(Math.random() * 12) + 1,
+    Math.floor(Math.random() * 31) + 1
+  );
+  person.setData('birthday', birthday);
+  
+  people.push(person);
+
+  return person;
+}
+
+function despawn_person(index) {
+  if (index < people.length) console.error("out of range index given");
+  people[index].destroy();
+  people.splice(index, 1);
+}
+
+
+
+const gui = new dat.GUI();
+
+window.walking = true;
+gui.add(window, "walking")
+
+window.walking_speed = 0.1;
+gui.add(window, "walking_speed", 0.01, 1.0, 0.01);
+
+window.amount_of_people = 32;
+gui.add(window, "amount_of_people", 2, 1000, 1);
+
+window.solve_paradox = function () {
+
+}
+gui.add(window, "solve_paradox");
+
 
 
 const game = new Phaser.Game({
@@ -25,16 +80,16 @@ const game = new Phaser.Game({
 
 function preload() {
   this.load.spritesheet('person.walking', 'src/assets/person.walking.png', {
-    frameWidth: 37,
-    frameHeight: 45,
-    endFrame: 17
+    frameWidth: 32,
+    frameHeight: 32,
+    endFrame: 0
   });
 }
 
 
 function create() {
   this.anims.create({
-    key: 'run',
+    key: 'walking',
     frames: 'person.walking',
     frameRate: 20,
     repeat: -1
@@ -44,31 +99,15 @@ function create() {
     font: '16px Courier',
     fill: '#00ff00'
   });
+  hint.depth = 1;
 
-  for (let p = 0; p < 25; p++) {
-    let person = this.add.sprite(
-      Math.floor(Math.random() * this.cameras.main.width),
-      Math.floor(Math.random() * this.cameras.main.height),
-      'person.walking').play('run').setInteractive();
 
-    var birthday = new Date(
-      Math.floor(Math.random() * 69) + 1950,
-      Math.floor(Math.random() * 12) + 1,
-      Math.floor(Math.random() * 31) + 1
-    );
-    person.setData('birthday', birthday);
-
-    var direction = Math.floor(Math.random() * 360);
-    person.flipX = direction >= 180
-    person.setData('direction', direction);
-
-    people.push(person);
+  for (let amount = 0; amount < amount_of_people; amount++) {
+    spawn_person(this);
   }
 
-  this.input.on('gameobjectup', function (pointer, person) {
-    if (selected) selected.setTint(0xffffff);
-    selected = person;
 
+  this.input.on('gameobjectmove', function (pointer, person) {
     hint.setText(person.getData('birthday').toLocaleDateString(
       undefined, {
         weekday: 'long',
@@ -77,16 +116,33 @@ function create() {
         day: 'numeric'
       }
     ));
-    person.setTint(0x98ff75);
+
+    person.setTint(0xa3f3ff);
+  });
+
+  this.input.on('gameobjectout', function (pointer, person) {
+    hint.setText("");
+
+    person.setTint(person.getData('color'));
   });
 }
 
 
 function update(time, delta) {
-  people.forEach(function (person, index) {
-    person.x += Math.cos(person.getData('direction') * Math.PI / 180) * 0.1 * delta;
-    person.y += Math.sin(person.getData('direction') * Math.PI / 180) * 0.1 * delta;
-  });
+  while (amount_of_people != people.length) {
+    if (amount_of_people > people.length) {
+      spawn_person(this);
+    } else {
+      despawn_person(people.length - 1);
+    }
+  }
 
-  Phaser.Actions.WrapInRectangle(people, Phaser.Geom.Rectangle.Clone(this.cameras.main), 45);
+  if (walking) {
+    people.forEach(function (person, index) {
+      person.x += Math.cos(person.getData('direction') * Math.PI / 180) * window.walking_speed * delta;
+      person.y += Math.sin(person.getData('direction') * Math.PI / 180) * window.walking_speed * delta;
+    });
+
+    Phaser.Actions.WrapInRectangle(people, Phaser.Geom.Rectangle.Clone(this.cameras.main), 45);
+  }
 }
